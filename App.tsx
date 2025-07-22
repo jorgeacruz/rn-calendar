2001b12a2fafc76e9059ebd7c87c77b8d628b608
Jorge Cruz <j.alexcruz@gmail.com>
2025-07-22 19:43:25 -0300

    Customize the calendar appearance and theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Calendar } from 'react-native-calendars';  
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true} />
+      <Text style={styles.Title}>
+        My Personal Calendar</Text>
+      <Calendar
+        style={styles.calendar}
+        // Customize the calendar appearance
+        headerStyle={{
+          borderBottomWidth: 1,
+          borderBottomColor: '#d6d7da',
+          paddingBottom: 10,
+          marginBottom: 10,
+        }}
+        // Customize the calendar theme
+        theme={{
+          monthTextColor: '#ffffff',
+          textMonthFontWeight: 'bold',
+          textMonthFontSize: 20,
+          todayTextColor: '#f53100',
+          selectedDayBackgroundColor: '#f53100',
+          selectedDayTextColor: '#ffffff',
+        }}/>
     </View>
   );
 }
@@ -13,8 +32,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1f023a',
+    paddingTop:50,
+    paddingHorizontal: 20,
   },
+  calendar:{  
+    backgroundColor:'transparent',
+  },
+  Title:{
+    color: '#fff',
+    fontSize: 24,
+    marginVertical: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  }
+
 });
